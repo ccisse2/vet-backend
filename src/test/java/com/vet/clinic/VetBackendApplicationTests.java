7548b2c2015c4731de056856bd4962d09552d8cf
cisse.cheikh@outlook.fr <cheikh.cisse2024@campus-eni.fr>
2025-04-09 16:00:22 +0200

    Initial commit - backend Spring Boot Dockerisé

---
+++ src/test/java/com/vet/clinic/VetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
